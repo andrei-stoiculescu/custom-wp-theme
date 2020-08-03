01b2c5ebd0faf75e58756ac56cac24d7d359016b
andrei-stoiculescu <andrei9d@gmail.com>
2020-08-03 09:07:25 +0300

    full height header added

--- wp-content/themes/custom-theme/header.php
+++ wp-content/themes/custom-theme/header.php
@@ -32,34 +32,20 @@
 
   <body>
 
+<div class="header-container-text header-overlay h-100">
+  <div class="d-flex align-items-center flex-column justify-content-center h-100 p-4">
+    <h1 class="display-4">Discover Romania</h1>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard.</p>
+    <button type="button" class="btn custom-btn">Book now</button>
+  </div>
+</div>
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    <div id="owl-header" class="owl-carousel owl-theme">
-     
-      <div class="item"><img src="https://images.unsplash.com/photo-1534371020656-6b85825f2b1a" alt="The Last of us">
-      </div>
-      <div class="item"><img src="https://images.unsplash.com/photo-1518887668165-8fa91a9178be" alt="GTA V"></div>
-      <div class="item"><img src="https://images.unsplash.com/photo-1531497825193-b208e39d008f" alt="Mirror Edge"></div>
-     
-    </div>
+<div id="owl-header" class="owl-carousel owl-theme">
+  <div class="owl-slide cover" style="background-image: url(https://emerging-europe.com/wp-content/uploads/2019/03/bigstock-gorgeous-spring-countryside-la-280866130-990x556.jpg);"></div>
+  <div class="owl-slide cover" style="background-image: url(https://booksandbao.com/wp-content/uploads/2020/01/majkl-velner-nKY59_d9FlA-unsplash-1600x1067.jpg);"></div>
+  <div class="owl-slide cover" style="background-image: url(https://cdn.kimkim.com/files/a/content_articles/featured_photos/32efe52768060fa172de0f052d3527f53cdfb58a/big-0c5409996c6e87912a9401bb9695d679.jpg);"></div>
+</div>
 
 
 
--- wp-content/themes/custom-theme/owlcarousel/header.js
+++ wp-content/themes/custom-theme/owlcarousel/header.js
@@ -1,13 +1,18 @@
 
 $(document).ready(function() {
   $("#owl-header").owlCarousel({
+    dots: false,
     items : 1, 
     loop : true,
     autoplay:true,
-    autoplayTimeout:3000,
-    autoplaySpeed: 1000,
+    autoplayTimeout:4000,
+    autoplaySpeed: 800,
     animateIn: 'fadeIn',
-    animateOut: 'fadeOut' 
+    animateOut: 'fadeOut',
+    mouseDrag: false,
+    touchDrag: false,
+    pullDrag: false,
+    freeDrag: false
   });
      
 });
